2d98b9c8672c996f7651104749580733df085dcb
Vaishali <vaishalithakur614@gmail.com>
2020-08-30 15:46:02 +0530

    Update 2. KMP Algo.cpp

--- Day 16 - Strings/2. KMP Algo.cpp
+++ Day 16 - Strings/2. KMP Algo.cpp
@@ -1,11 +1,11 @@
 int  kmp(string S, string P){
 	int n = P.size();
-  int m = S.size();
-  
-  if(n == 0)	
-    return 0;
-  if(m == 0)	
-    return -1;
+	int m = S.size();
+
+	if(n == 0)	
+		return 0;
+	if(m == 0)	
+		return -1;
 
 	int prefix[n];
 	prefix[0] = 0;
@@ -23,7 +23,7 @@ int  kmp(string S, string P){
 				j = prefix[j - 1];
 		}
 	}
-  
+
 	// searching begins
 	int i, j = 0; // iterator for the pattern
 	for(i = 0; i < m; ){
@@ -32,14 +32,14 @@ int  kmp(string S, string P){
 			j++; 
 		} 
     
-    if(j == n){
-      return (i - j);
-      j = prefix[j - 1];
-    } else if(i < m && S[i] != P[j]){
+		if(j == n){
+			return (i - j);
+			j = prefix[j - 1];
+		} else if(i < m && S[i] != P[j]){
 			if(j == 0) 	
-        i++;
-      else 	
-        j = prefix[j - 1];
+				i++;
+			else 	
+				j = prefix[j - 1];
 		}
 	}
 	return -1;
